4d66df9a92a40850ab63406e6b0d65d5e5233c9a
Brian Koser <brian@koser.us>
2023-09-29 16:30:26 -0500

    Youtube shortcode update

--- _includes/shortcodes/youtube.js
+++ _includes/shortcodes/youtube.js
@@ -4,16 +4,10 @@ module.exports = function (data) {
         videoEmbed = '<p>Video unavailable!</p>';
     }
     else if (data.id != undefined) {
-        videoEmbed = `
-<div class="video-embed-container">
-    <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/${data.id}${data.timestamp == undefined ? '' : `?t=${timestampToYouTubeTimestamp(data.timestamp)}`}" frameborder="0" allowfullscreen></iframe>
-</div>`;
+        videoEmbed = `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/${data.id}${data.timestamp == undefined ? '' : `?t=${timestampToYouTubeTimestamp(data.timestamp)}`}" frameborder="0" allowfullscreen></iframe>`;
     }
     else if (data.playlistID != undefined) {
-        videoEmbed = `
-<div class="video-embed-container">
-    <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>
-</div>`;
+        videoEmbed = `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>`;
     }
 
     return `
