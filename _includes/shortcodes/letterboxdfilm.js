a69eb2c399d5f6fa2306757b8e0100e736fc6e9f
Brian Koser <brian@koser.us>
2024-04-26 16:27:26 -0500

    Remove test description

--- _includes/shortcodes/letterboxdfilm.js
+++ _includes/shortcodes/letterboxdfilm.js
@@ -1,6 +1,6 @@
 module.exports = function (data) {
     return `<div class="film">
-    <a class="cover" href="${data.link}" data-description="${data.description}">
+    <a class="cover" href="${data.link}">
         <img loading="lazy" width="160" height="240" src="${data.posterUrl}" alt="${data.filmTitle}" title="${data.filmTitle}">
     </a>
 </div>`;
