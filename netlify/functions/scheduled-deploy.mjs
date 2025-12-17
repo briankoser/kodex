2f9555b4baa0a4e1d73bd16a2f68eef10fb6839c
Brian Koser <brian@koser.us>
2025-12-17 16:30:58 -0600

    Scheduled deploy

--- netlify/functions/scheduled-deploy.mjs
+++ netlify/functions/scheduled-deploy.mjs
@@ -1,6 +1,14 @@
-export default async (req) => {
-    const { next_run } = await req.json()
+import fetch from 'node-fetch'
 
+export default async (req) => {
+    const BUILD_HOOK = 'https://api.netlify.com/build_hooks/664ce70493cb4b06ea4d6d38'
+    await fetch(BUILD_HOOK, {
+        method: 'POST'
+    }).then(response => {
+        console.log('Build hook response:', response)
+    });
+
+    const { next_run } = await req.json()
     console.log("Received event! Next invocation at:", next_run)
 }
 
